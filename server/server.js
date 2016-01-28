c3eec252ed1c2924cdeec27e4feb08605d8a9bf4
midea <278931058@qq.com>
2016-01-28 20:07:31 +0800

    前端页面

--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var path = require('path');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+app.start = function () {
+    // start the web server
+    return app.listen(function () {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            var explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+    });
 };
+
+//var staticPath = null;
+//
+//staticPath = path.resolve(__dirname, '../client/');
+//console.log("Running app in development mode");
+//
+//app.use(loopback.static(staticPath));
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+boot(app, __dirname, function (err) {
+    if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    // start the server if `$ node server.js`
+    if (require.main === module)
+        app.start();
 });
