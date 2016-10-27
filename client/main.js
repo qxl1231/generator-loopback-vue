c8a31b4a81582171255182b28ebf82da75a674dd
Johnson <278931058@qq.com>
2016-10-27 14:32:02 +0800

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,16 +1,40 @@
-var Vue = require('vue')
-var App = require('./table.vue')
-var Version = require('./version.vue')
-var Login = require('./login.vue')
+var Vue = require('vue');
+var table = require('./table.vue');
+var Version = require('./version.vue');
+var login = require('./login.vue');
+var select = require('./select.vue');
+var app_version = require('./app_version.vue');
+var app_table = require('./app_table.vue');
 
+var VueRouter = require('vue-router');
 
 Vue.use(require('vue-resource'));
+Vue.use(VueRouter);
 
-new Vue({
-    el: 'body',
-    components: {
-        app: App,
-        version: Version,
-        login:Login
+var router = new VueRouter();
+
+var app = Vue.extend({});
+
+router.map({
+    '/': {        //登录
+        component: login
+    },
+    '/login': {
+        component: login
+    },
+    /* 404路由 */
+    '*': {
+        component: login
+    },
+    '/appversion': {               //主页
+        component: table
+    },
+    '/select': {
+        component: select
+    },
+    '/app_version': {               //主页
+        component: app_table
     }
-})
+});
+
+router.start(app, "#app");
