58e80ef4f45a3a976f863955186c619d51c693c9
Johnson <278931058@qq.com>
2016-11-11 18:10:00 +0800

    Update login.vue

--- client/login.vue
+++ client/login.vue
@@ -151,7 +151,7 @@
                 var settings = {
                     "async": true,
                     "crossDomain": true,
-                    "url": "/api/v1/Users/login",
+                    "url": "localhost:4000/api/v1/Users/login",
                     "method": "POST",
                     "headers": {
                         "content-type": "application/json",
