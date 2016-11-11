c3b07c1d6ff900704f6ae547925653fab3de354e
Johnson <278931058@qq.com>
2016-11-11 18:38:31 +0800

    Update bundle.js

--- client/bundle.js
+++ client/bundle.js
@@ -316,7 +316,7 @@ module.exports = {
             var settings = {
                 "async": true,
                 "crossDomain": true,
-                "url": "http://qxl1231-lb-vue3.daoapp.io:4000/api/v1/Users/login",
+                "url": "qxl1231-lb-vue3.daoapp.io:4000/api/v1/Users/login",
                 "method": "POST",
                 "headers": {
                     "content-type": "application/json",
